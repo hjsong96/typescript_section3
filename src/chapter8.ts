955c532d167a4d6dd615801a68a414f91ec7b8a9
hjsong96 <ghkwlsdbwls@gmail.com>
2025-08-20 17:14:59 +0900

    추가 변동사항 커밋

--- src/chapter8.ts
+++ src/chapter8.ts
@@ -4,27 +4,38 @@
  */
 
 type Admin = {
+    tag: 'ADMIN';
     name: string;
     kickCount: number;
 };
 
 type Member = {
+    tag: 'MEMBER';
     name: string;
     point: number;
 };
 
 type Guset = {
+    tag: 'GUEST';
     name: string;
     visitCount: number;
 };
 
 type User = Admin | Member | Guset;
 
-// Admin -> {name}님 현재까지 {kickCount} 명 강퇴했습니다.
-// Member -> {name}님 현재까지 {point} 모았습니다.
-// Guset -> {name}님 현재까지 {visitCount} 번 오셨습니다.
 function login(user: User) {
-    if ('kickCount' in user) {
-        // admin 타입
-    } else if ()
+    switch (user.tag) {
+        case 'ADMIN': {
+            console.log(`${user.name}님 현재까지 ${user.kickCount} 명 강퇴했습니다.`);
+            break;
+        }
+        case 'MEMBER': {
+            console.log(`${user.name}님 현재까지 ${user.point} 모았습니다.`);
+            break;
+        }
+        case 'GUEST': {
+            console.log(`${user.name}님 현재까지 ${user.visitCount} 번 오셨습니다.`);
+            break;
+        }
+    }
 }
