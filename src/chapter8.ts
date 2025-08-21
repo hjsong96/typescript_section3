3032065b6cd02d11fcd2367227bc9f35c05c851a
hjsong96 <ghkwlsdbwls@gmail.com>
2025-08-22 08:54:37 +0900

    타입스크립트 이해 섹션 최종완료

--- src/chapter8.ts
+++ src/chapter8.ts
@@ -39,3 +39,64 @@ function login(user: User) {
         }
     }
 }
+
+/**
+ * 복습겸 한 가지 사례
+ */
+
+// 비동기 작업의 결과를 처리하는 객체
+
+type LoadingTask = {
+    state: 'LOADING';
+};
+
+type FailedTask = {
+    state: 'FAILED';
+    error: {
+        message: string;
+    };
+};
+
+type SuccessTask = {
+    state: 'SUCCESS';
+    response: {
+        data: string;
+    };
+};
+
+type AsyncTask = LoadingTask | FailedTask | SuccessTask;
+
+// 로딩 중 -> 콘솔에 로딩중 출력
+// 실패 -> 실패 : 에러 메시지를 출력
+// 성공 -> 성공 : 데이터를 출력
+function processResult(task: AsyncTask) {
+    switch (task.state) {
+        case 'LOADING':
+            console.log('로딩중');
+            break;
+        case 'FAILED':
+            console.log(`에러 발생: ${task.error.message}`);
+            break;
+        case 'SUCCESS':
+            console.log(`성공: ${task.response.data}`);
+            break;
+    }
+}
+
+const loading: AsyncTask = {
+    state: 'LOADING',
+};
+
+const failed: AsyncTask = {
+    state: 'FAILED',
+    error: {
+        message: '오류 발생 원인은',
+    },
+};
+
+const success: AsyncTask = {
+    state: 'SUCCESS',
+    response: {
+        data: '데이터 ~~',
+    },
+};
